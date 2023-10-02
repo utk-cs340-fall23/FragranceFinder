4716573271eddfba7c0b092ecac4700a43b104eb
OutliersAdvantage <102266508+LakelonBailey@users.noreply.github.com>
2023-10-01 21:43:42 -0400

    added some more comments on the frontend and simplified requests code

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,40 +1,49 @@
 import decode from 'jwt-decode';
 
 class AuthService {
-  getProfile() {
-    return decode(this.getToken());
-  }
 
-  loggedIn() {
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
-  }
-
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
+    // Return user information found in the token
+    getProfile() {
+        return decode(this.getToken());
     }
-  }
 
-  getToken() {
-    return localStorage.getItem('id_token');
-  }
+    // If token exists and is not expired, user is logged in
+    loggedIn() {
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    }
 
-  login(idToken) {
-    localStorage.setItem('id_token', idToken);
+    // Check if token is expired against its
+    // timestamp found in 'exp'
+    isTokenExpired(token) {
+        try {
+        const decoded = decode(token);
+        if (decoded.exp < Date.now() / 1000) {
+            return true;
+        } else return false;
+        } catch (err) {
+        return false;
+        }
+    }
 
-    window.location.assign('/');
-  }
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
 
-  logout() {
-    localStorage.removeItem('id_token');
-    window.location.assign('/');
-  }
+    // "log in" the user by setting token in local storage
+    // and redirecting to home page
+    login(idToken) {
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/');
+    }
+
+    // "log out" the user by removing local storage token
+    // and redirecting to home page
+    logout() {
+        localStorage.removeItem('id_token');
+        window.location.assign('/');
+    }
 }
 
 const instance = new AuthService();
--- frontend/src/utils/requests.js
+++ frontend/src/utils/requests.js
@@ -1,41 +1,48 @@
-const sendGet = async (url) => {
-    const token = localStorage.getItem('id_token');
-    const res = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            "Authorization": token ? `Bearer ${token}` : '',
-        },
-    });
+import AuthService from '../utils/auth';
 
+
+const getDefaultHeaders = () => {
+    const token = AuthService.getToken();
+    return {
+        'Accept': 'application/json',
+        "Authorization": token ? `Bearer ${token}` : '',
+    }
+}
+
+
+const setData = res => {
     const contentType = res.headers.get("content-type");
     if (contentType && contentType.includes('application/json')) {
         const data = await res.json()
         res.data = data;
     }
-
     return res;
 }
 
+// Default method to send a GET request
+const sendGet = async (url) => {
+    const res = await fetch(url, {
+        method: 'GET',
+        headers: {
+            ...getDefaultHeaders()
+        },
+    });
+
+    return setData(res);
+}
+
+// Default method to send a GET request
 const sendPost = async (url, data) => {
-    const token = localStorage.getItem('id_token');
     const res = await fetch(url, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
-        'Accept': 'application/json',
+        ...getDefaultHeaders(),
         'Content-Type': 'application/json',
-        "Authorization": token ? `Bearer ${token}` : '',
       }
     })
 
-    const contentType = res.headers.get("content-type");
-    if (contentType && contentType.includes('application/json')) {
-        const data = await res.json()
-        res.data = data;
-    }
-
-    return res
+    return setData(res);
 }
 
 module.exports = {
