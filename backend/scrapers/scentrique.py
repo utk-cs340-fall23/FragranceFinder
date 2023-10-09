f123a6272f4ca4082a49d2da644b97718d089a81
ssouther <ssouthe3@vols.utk.edu>
2023-10-09 15:15:11 -0400

    Update scentrique.py

--- backend/scrapers/scentrique.py
+++ backend/scrapers/scentrique.py
@@ -1,10 +1,8 @@
+import pandas as pd
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
 with sync_playwright() as p:
-
-    linksToScrape = []
-
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto('https://www.scentrique.com/fragrances')
@@ -13,7 +11,8 @@ with sync_playwright() as p:
     ul_list = soup.find('ul', class_='S4WbK_ uQ5Uah c2Zj9x H1ux6p')
 
     for i in ul_list:
-        #linksToScrape.append(i.find("a", class_="oQUvqL x5qIv3")["href"])
-        subbrowser = p.chromium.launch(headless=False)
-        subpage = subbrowser.new_page()
-        subpage.goto(i.find("a", class_="oQUvqL x5qIv3")["href"])
+        page.goto(i.find("a", class_="oQUvqL x5qIv3")["href"])
+        html = page.inner_html('#TPAMultiSection_jqwvt9ch')
+        soup = BeautifulSoup(html, 'html.parser')
+        name = soup.find('h1', class_='_2qrJF')
+        print(name.decode_contents())
