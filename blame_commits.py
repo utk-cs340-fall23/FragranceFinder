a41890903a94e39c983d2a1549f856c07a167c2b
LakelonBailey <lake.bailey@icloud.com>
2023-10-11 17:30:03 -0400

    simplified blame_commits.py

--- blame_commits.py
+++ blame_commits.py
@@ -30,7 +30,11 @@ def recursive_git_blame(directory, username, ignore_dirs=[], ignore_files=[], ig
                 continue
 
             file_path = os.path.join(root, f)
-            unique_files.add(file_path)
+            unique_files.add(
+                file_path
+                .replace('./', '')
+                .replace('.\\', '')
+            )
             blame_output = git_blame_for_file(file_path, username)
             if blame_output:
                 results.append(blame_output)
@@ -43,28 +47,19 @@ if __name__ == "__main__":
     username = input("Enter your GitHub username: ").lower()
     sprint_num = int(input("Enter the sprint number: "))
 
-    directories_to_ignore = ['venv', 'node_modules', 'public', 'build', '__pycache__']
-    files_to_ignore = ['package-lock.json', '.gitignore']
+    directories_to_ignore = ['venv', 'node_modules', 'public', 'build', '__pycache__', '.git']
+    files_to_ignore = ['package-lock.json', '.gitignore', '.DS_Store']
     extensions_to_ignore = ['svg', 'env']
-
-    frontend_result, frontend_files = recursive_git_blame(
-        'frontend',
+    result, files = recursive_git_blame(
+        '.',
         username,
         ignore_dirs=directories_to_ignore,
         ignore_files=files_to_ignore,
         ignore_extensions=extensions_to_ignore
     )
 
-    backend_result, backend_files = recursive_git_blame(
-        'backend',
-        username,
-        ignore_dirs=directories_to_ignore,
-        ignore_files=files_to_ignore,
-        ignore_extensions=extensions_to_ignore
-    )
-    result = frontend_result + backend_result
-    all_files = "\n".join(frontend_files.union(backend_files))
+    files_string = "\n".join(files)
     with open(f"sprint{sprint_num}/{netid}.commits.txt", 'w') as fl:
         fl.write(result)
 
-    print(f"\nFiles worked on:\n{all_files}")
+    print(f"\nFiles worked on:\n{files_string}")
