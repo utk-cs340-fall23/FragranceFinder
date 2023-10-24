436e4dc06aae00d20cdae9fce609054decdd93dc
LakelonBailey <lake.bailey@icloud.com>
2023-10-24 11:53:10 -0400

    made master script push data to csv

--- backend/scrapers/MasterScript.py
+++ backend/scrapers/MasterScript.py
@@ -1,9 +1,14 @@
 import asyncio
 import pandas as pd
 from jomashop import scrape_jomashop
+import os
+
+# Create data directory if it doesn't exist
+if not os.path.exists(os.path.join(os.getcwd(), 'data')):
+    os.mkdir('data', 0x774)
+
 
 temp = asyncio.run(scrape_jomashop())
-
 df = pd.read_json(temp, orient="columns")
-
+df.to_csv('data/jomashop.csv', index=False)
 print(df)
