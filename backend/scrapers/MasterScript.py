5866e6e2d55408202a0be71d19065bc1b5ea87ff
ncoffey3 <ncoffey3@vols.utk.edu>
2023-10-27 00:21:24 -0400

    MasterScript runs scrapers async

--- backend/scrapers/MasterScript.py
+++ backend/scrapers/MasterScript.py
@@ -8,6 +8,12 @@ import sys
 # Create data directory if it doesn't exist
 #if not os.path.exists(os.path.join(os.getcwd(), 'data')):
 #    os.mkdir('data', 0x774)
+async def get_data(max_items):
+    jomashopData, maxaromaData = await asyncio.gather(
+        scrape_jomashop(max_items),
+        scrape_maxaroma(max_items)
+    )
+    return jomashopData, maxaromaData
 
 
 max_items_per_scraper = 10000
@@ -19,9 +25,13 @@ if len(sys.argv) > 1:
             f"Invalid integer provided: '{sys.argv[1]}'. Defaulting to {max_items_per_scraper} as max items per scraper"
         )
 
+jomashopData, maxaromaData = asyncio.run(get_data(max_items_per_scraper))
+
 frames = [
-    pd.read_json(asyncio.run(scrape_jomashop(max_items_per_scraper)), orient="records"),
-    pd.read_json(asyncio.run(scrape_maxaroma(max_items_per_scraper)), orient="records")
+    #pd.read_json(asyncio.run(scrape_jomashop(max_items_per_scraper)), orient="records"),
+    #pd.read_json(asyncio.run(scrape_maxaroma(max_items_per_scraper)), orient="records")
+    pd.read_json(jomashopData, orient="records"),
+    pd.read_json(maxaromaData, orient="records")
 ]
 
 gdf = pd.concat(frames)
